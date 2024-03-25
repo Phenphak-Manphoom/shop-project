43a2f509457205da9f46073d7c86b324ce0feb35
Tae <phenphak.forlearn@gmail.com>
2024-03-25 15:58:33 +0700

    set up project connect db

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import productRoutes from "./routes/products.js";
+import { connectDatabase } from "./config/dbConnect.js";
+
+const app = express();
+
+dotenv.config({ path: "backend/config/config.env" });
+connectDatabase();
+app.use("/api/v1", productRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
