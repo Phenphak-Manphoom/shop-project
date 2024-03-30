51057b68540a0664cd0701aa44cb04bbd083b7fc
Tae <phenphak.forlearn@gmail.com>
2024-03-30 11:24:54 +0700

    error handling

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,35 @@ import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/products.js";
 import { connectDatabase } from "./config/dbConnect.js";
+import errorMiddleware from "./middlewares/error.js";
 
+//handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
 const app = express();
-
+console.log("hello");
 dotenv.config({ path: "backend/config/config.env" });
 connectDatabase();
 app.use(express.json());
 app.use("/api/v1", productRoutes);
 
-app.listen(process.env.PORT, () => {
+//using error middleware
+app.use(errorMiddleware);
+
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server started on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+//handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err}`);
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
