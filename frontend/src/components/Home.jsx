3666b3aff2f5885c290d1b100a9d3b6cec27c2c1
Tae <phenphak.forlearn@gmail.com>
2024-04-29 18:19:04 +0700

    add pagination search and filter

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,9 +4,27 @@ import { useGetProductsQuery } from "../redux/api/productApi";
 import ProductItem from "./product/ProductItem";
 import Loader from "./layout/Loader";
 import toast from "react-hot-toast";
+import CustomPagination from "./layout/CustomPagination";
+import { useSearchParams } from "react-router-dom";
+import Filter from "./layout/Filter";
 
 const Home = () => {
-  const { data, isLoading, error, isError } = useGetProductsQuery();
+  let [searchParams] = useSearchParams();
+  const page = searchParams.get("page") || 1;
+  const keyword = searchParams.get("keyword") || "";
+  const min = searchParams.get("min");
+  const max = searchParams.get("max");
+  const category = searchParams.get("category");
+  const ratings = searchParams.get("ratings");
+
+  const params = { page, keyword };
+
+  min !== null && (params.min = min);
+  max !== null && (params.max = max);
+  category !== null && (params.category = category);
+  ratings !== null && (params.ratings = ratings);
+
+  const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
   useEffect(() => {
     if (isError) {
@@ -14,23 +32,36 @@ const Home = () => {
     }
   }, [isError]);
 
+  const columnSize = keyword ? 4 : 3;
+
   if (isLoading) return <Loader />;
   return (
     <>
       <MetaData title={"Buy Best Product Online"} />
       <div className="row">
-        <div className="col-12 col-sm-6 col-md-12">
+        {keyword && (
+          <div className="col-6 col-md-3 mt-5">
+            <Filter />
+          </div>
+        )}
+        <div className={keyword ? "col-6 col-md-9" : "col-6 col-md-12"}>
           <h1 id="products_heading" className="text-secondary">
-            Latest Products
+            {keyword
+              ? `${data?.products?.length} Products found with keyword: ${keyword}`
+              : "Latest Products"}
           </h1>
 
           <section id="products" className="mt-5">
             <div className="row">
               {data?.products?.map((product) => (
-                <ProductItem product={product} />
+                <ProductItem product={product} columnSize={columnSize} />
               ))}
             </div>
           </section>
+          <CustomPagination
+            resPerPage={data?.resPerPage}
+            filteredProductsCount={data?.filteredProductsCount}
+          />
         </div>
       </div>
     </>
