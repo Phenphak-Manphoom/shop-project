5cdeda7a04a9d603541860e0135d69d58d20f114
Tae <phenphak.forlearn@gmail.com>
2024-05-13 15:51:44 +0700

    config upload avatar

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 console.log("hello");
 dotenv.config({ path: "backend/config/config.env" });
 connectDatabase();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser())
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -5,6 +5,8 @@ import ErrorHandler from "../utils/errorHandler.js";
 import sendToken from "../utils/sendToken.js";
 import sendEmail from "../utils/sendEmail.js";
 import crypto from "crypto";
+import { delete_file, upload_file } from "../utils/cloudnary.js";
+
 //register user => /api/v1/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -48,6 +50,24 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     message: "Logged Out",
+  });
+});
+
+// Upload user avatar   =>  /api/v1/me/upload_avatar
+export const uploadAvatar = catchAsyncErrors(async (req, res, next) => {
+  const avatarResponse = await upload_file(req.body.avatar, "shopit/avatars");
+
+  // Remove previous avatar
+  if (req?.user?.avatar?.url) {
+    await delete_file(req?.user?.avatar?.public_id);
+  }
+
+  const user = await User.findByIdAndUpdate(req?.user?._id, {
+    avatar: avatarResponse,
+  });
+
+  res.status(200).json({
+    user,
   });
 });
 
